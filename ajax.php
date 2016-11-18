7b18b8ed94ed56647fc43e14166211f5a44e7ce9
Andrés Nacimiento <andres@opencodeconsulting.com>
2016-11-18 12:21:44 +0000

    Ajax

---
+++ ajax.php
@@ -0,0 +1,14 @@
+<?php
+	/* Includes */
+	include "EventsValidationController.php";
+	
+	/* Ajax options */
+	switch ($_POST['action']){
+		case "eventsValidation":
+			$event = new EventValidation();
+			echo $event->check();
+			break;
+		default:
+			echo "Error: action param incorrect.";
+	}
+?>
