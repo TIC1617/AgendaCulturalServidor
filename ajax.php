e48570d326fb086753e42ae0e08282e257e8e011
Andrés Nacimiento <andres@opencodeconsulting.com>
2016-11-18 13:43:53 +0000

    DGP1617-11 Backend-end: Login

--- ajax.php
+++ ajax.php
@@ -1,12 +1,17 @@
 <?php
 	/* Includes */
 	include "controllers/EventsValidationController.php";
+	include "controllers/LoginController.php";
 	
 	/* Ajax options */
 	switch ($_POST['action']){
 		case "eventsValidation":
 			$event = new EventValidation();
 			echo $event->check();
+			break;
+		case "loginValidation":
+			$login = new Login();
+			echo $login->check();
 			break;
 		default:
 			echo "Error: action param incorrect.";
